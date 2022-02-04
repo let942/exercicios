bf2e123072b11e55ca78d0058604a52bc08e7ec9
Letícia Chagas Marques <let942@gmail.com>
2022-02-04 14:39:54 -0300

    adicona exercícios de array e loop for

---
+++ fundamentos/bloco-4-introducao-a-JavaScript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -0,0 +1,100 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1. percorrer o array e imprimindo todos os valores neles contidos
+//console.log(numbers)
+
+//2. somar todos os valores e imprimir o resultado
+// let soma = 0;
+// for( let i = 0; i < numbers.length; i +=1){
+//   soma = soma + numbers[i];
+// }
+// console.log(soma)
+
+//3. calcular e mostrar média
+// let soma = 0;
+// for( let i = 0; i < numbers.length; i +=1){
+//   soma = soma + numbers[i];
+// }
+// media = soma/(numbers.length)
+// console.log(media)
+
+//4. se a média for maior que 20, imprima "valor maior que 20", se não "valor menor ou igual a 20"
+// let soma = 0;
+// for( let i = 0; i < numbers.length; i +=1){
+//   soma = soma + numbers[i];
+// }
+// media = soma/(numbers.length)
+// console.log(media)
+// if(media>20){
+//   console.log("valor maior que 20");
+// }
+// else{
+//   console.log("valor menor ou igual a 20");
+// }
+
+//5. descobrir maior valor e imprimi-lo
+// let maiorValor = 0;
+// for( let i = 0; i < numbers.length; i +=1){
+//  if(maiorValor<numbers[i]){
+//    maiorValor = numbers[i]; 
+//  }
+//  }
+// console.log(maiorValor);
+
+//6. quantos valores ímpares e imprima o resultado
+// let valoresImpares = [0];
+// let numeroDeImpares = 0;
+// for( let i = 0; i < numbers.length; i +=1){
+//  if(numbers[i]%2 === 1){
+//    valoresImpares.push(numbers[i]);
+//    numeroDeImpares = numeroDeImpares +1;
+//  }
+//  }
+// console.log(valoresImpares);
+// console.log(numeroDeImpares);
+
+//7. descobrir menor valor e imprimi-lo
+//  let menorValor = 0;
+//  let maiorValor = 0;
+// for( let i = 0; i < numbers.length; i +=1){
+//  if(maiorValor<numbers[i]){
+//    maiorValor = numbers[i]; 
+//  }
+//  }
+//  for( let i = 0; i < numbers.length; i +=1){
+//   if(maiorValor>numbers[i]){
+//     menorValor = numbers[i]; 
+//     maiorValor = numbers[i];
+//   }
+//   }
+// console.log(menorValor);
+
+//8. um array de 1 a 25 e imprima o resultado
+// let array = [];
+// let contador = 0;
+// for (let i = 0; i < numbers.length; i += 1 ){
+//   if(numbers[i]<25){
+//      contador = contador+ 1;
+//      array.push(numbers[i]);
+//   }
+//   }
+// console.log(contador);
+// console.log(array);
+
+//9.  use o array anterior e imprima o resultado de cada número dividido por 2
+// let array = [];
+// for (let i = 0; i < numbers.length; i += 1 ){
+//   if(numbers[i]<25){
+     
+//      array.push(numbers[i]);
+//   }
+// }
+// console.log(array);
+
+// let divisao = [];
+// let numeroDaVez = 0;
+// for (let j =0; j < array.length; j += 1){
+//   numeroDaVez = array[j]/2;
+//   divisao.push(numeroDaVez)
+// }
+// console.log(divisao);
